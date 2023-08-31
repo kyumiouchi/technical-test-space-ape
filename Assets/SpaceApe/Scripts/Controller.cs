f52489e38ba168522e002efeb646e3ec6d2b7e2b
Yumi Ouchi <yumiouchi@gmail.com>
2023-08-31 08:14:15 -0400

    Remove assets not used and Add used assets

---
+++ Assets/SpaceApe/Scripts/Controller.cs
@@ -0,0 +1,133 @@
+﻿using System;
+using UnityEngine;
+
+namespace DodgyBoxes
+{
+    public class Controller : MonoBehaviour
+    {
+        /// <summary>
+        /// States of the game.
+        /// </summary>
+        private enum GameStates
+        {
+            /// <summary>
+            /// Initial setup.
+            /// </summary>
+            Init,
+
+            /// <summary>
+            /// Player is in the main menu.
+            /// </summary>
+            MainMenu,
+
+            /// <summary>
+            /// Player is in the difficulty menu.
+            /// </summary>
+            DifficultyMenu,
+
+            /// <summary>
+            /// Player is playing the game.
+            /// </summary>
+            Game
+        }
+
+        /// <summary>
+        /// MainMenu component on the main menu GameObject.
+        /// </summary>
+        [SerializeField] private MainMenu mainMenu = null;
+
+        /// <summary>
+        /// DifficultyMenu component on the difficulty GameObject.
+        /// </summary>
+        [SerializeField] private DifficultyMenu difficultyMenu = null;
+
+        /// <summary>
+        /// Game component on the toys GameObject.
+        /// </summary>
+        [SerializeField] private Game game = null;
+
+        /// <summary>
+        /// The current state of the game.
+        /// </summary>
+        private GameStates state = GameStates.Init;
+
+        private void Start()
+        {
+            mainMenu.StartGame += OnStartGame;
+            difficultyMenu.DifficultySelected += OnDifficultySelected;
+            game.GameComplete += OnGameComplete;
+            State = GameStates.MainMenu;
+        }
+
+        /// <summary>
+        /// State of the game. Changing the state will transition the game.
+        /// </summary>
+        private GameStates State
+        {
+
+            get => state;
+
+            set
+            {
+                // Cannot return to init.
+                if (value == GameStates.Init)
+                {
+                    throw new ArgumentException("Cannot return to init state.");
+                }
+
+                state = value;
+
+                switch (state)
+                {
+                    case GameStates.MainMenu:
+                        mainMenu.gameObject.SetActive(true);
+                        difficultyMenu.gameObject.SetActive(false);
+                        game.gameObject.SetActive(false);
+                        break;
+
+                    case GameStates.DifficultyMenu:
+                        mainMenu.gameObject.SetActive(false);
+                        difficultyMenu.gameObject.SetActive(true);
+                        game.gameObject.SetActive(false);
+                        break;
+
+                    case GameStates.Game:
+                        mainMenu.gameObject.SetActive(false);
+                        difficultyMenu.gameObject.SetActive(false);
+                        game.gameObject.SetActive(true);
+                        break;
+
+                    case GameStates.Init:
+                    // Intentional fallthrough. Init isn't supported but will be explicitly handled above.
+                    default:
+                        throw new ArgumentOutOfRangeException($"State machine doesn't support state {state}.");
+                }
+            }
+        }
+
+        /// <summary>
+        /// Triggered when the player presses the start game button.
+        /// </summary>
+        private void OnStartGame()
+        {
+            State = GameStates.DifficultyMenu;
+            game.InitialiseGame();
+        }
+
+        /// <summary>
+        /// Triggered when the player chooses a difficulty.
+        /// </summary>
+        private void OnDifficultySelected()
+        {
+            State = GameStates.Game;
+        }
+
+        /// <summary>
+        /// Triggered when the player completes a game.
+        /// </summary>
+        private void OnGameComplete()
+        {
+            State = GameStates.MainMenu;
+        }
+    }
+}
---
+++ Assets/SpaceApe/Scripts/UI/DifficultyMenu.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using UnityEngine;
+
+namespace DodgyBoxes
+{
+    /// <summary>
+    /// DifficultyMenu component, responsible for allowing the player to select a difficulty.
+    /// </summary>
+    public class DifficultyMenu : MonoBehaviour
+    {
+        /// <summary>
+        /// Delegate triggered when a difficulty is selected.
+        /// </summary>
+        public Action DifficultySelected = delegate { };
+
+        /// <summary>
+        /// Triggered when the player selects a difficulty.
+        /// </summary>
+        public void OnDifficultySelectedButtonPressed()
+        {
+            DifficultySelected();
+        }
+    }
+}
